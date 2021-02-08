a255f05a4101d4d23db0106fa929fd412237ff6a
ricardocastillo <ricardocastilloisc@gmail.com>
2021-02-07 23:05:55 -0500

    fix de ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'video', loadChildren: () => import('./video/video.module').then(m => m.VideoModule) }];
+const routes: Routes = [
+  { path: 'video', loadChildren: () => import('./video/video.module').then(m => m.VideoModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'video'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
